986147cf6589002d717a701b6d481cc92fdab011
GustavoFreitas22 <gusfmotta@gmail.com>
2021-08-26 16:22:53 -0300

    components home, Sobre-nos and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { SobreNosComponent } from './sobre-nos/sobre-nos.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SobreNosComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
